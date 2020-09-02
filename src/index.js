bb25ca57519d87abb69866e26dbd6d1c743e554a
Junaid Atari <mj.atari@gmail.com>
2020-09-03 00:02:08 +0500

    import bootstrap css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 // @flow
 
+/**
+ * @author Junaid Atari <mj.atari@gmail.com>
+ * @since 2020-08-14
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+// Styles
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
 	<React.StrictMode>
